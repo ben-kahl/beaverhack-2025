9f383d3434d69fdc2b1debe51232719d6094e5ec
Ben Kahl <ben.kahl2002@gmail.com>
2025-04-05 18:03:12 -0700

    Quick game data loader class to load text from json data file

--- game_data_loader.gd
+++ game_data_loader.gd
@@ -1,9 +1,23 @@
-class_name GameDataProcessor
+class_name GameDataLoader
 
 var text_data
-var current_POS = null
+var current_pos = null
 
 func _init() -> void:
-	pass
+	text_data = loadJSONData()
+	current_pos = 'intro'
 	
-	
+func loadJSONData():
+	var data_file = FileAccess.open("res://data/data.json", FileAccess.READ)
+	var data_string = data_file.get_as_text()
+	var json_data = JSON.new()
+	var error = json_data.parse(data_string)
+	if error == OK:
+		var data_received = json_data.data
+		if typeof(data_received) == TYPE_DICTIONARY:
+			return data_received
+		else:
+			assert(false, "Unexpected data in JSON")
+	else:
+		print("JSON parser error: ", json_data.get_error_message(), "in", data_string, "at line", json_data.get_error_line())
+		assert(false, "JSON Parse Error")
